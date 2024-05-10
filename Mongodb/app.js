c695f7ce895f6db6fb1dd59c5ce25062441725e7
Bharti Verma <bhartivermak2012@gmail.com>
2024-05-10 17:53:05 +0530

    Mongodb saving and getting data

--- Mongodb/app.js
+++ Mongodb/app.js
@@ -1,44 +1,115 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
+const Blog = require("./models/blog");
 
-const app  = express(); 
+const app = express();
+
+// connect to mongodb and listen for requests when connected to db
+
+const dbURI =
+  "mongodb+srv://<username>:<password>@node-tuts.4fyd3ob.mongodb.net/<db_collection>?retryWrites=true&w=majority&appName=Node-tuts";
+
+mongoose
+  .connect(dbURI)
+  .then((result) => {
+    app.listen(3000, () => {
+      console.log("Connected to Mongodb");
+      console.log("listining to port 3000");
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 // register ejs
-app.set('view engine','ejs');
+app.set("view engine", "ejs");
 
-app.listen(3000,() => {
-    console.log('listining to port 3000');
-})
-
-// custom middleware to log req info middleware 
-app.use((req,res,next) => {
-    console.log("path :",req.path);
-    console.log("hostname :",req.hostname);
-    console.log("method :",req.method);
-    next();  // next function is used to tell node to move on to next bit of code explicitly as node by default doesn't continue executing with custom middleware like this 
-})
-
+// custom middleware to log req info middleware
+app.use((req, res, next) => {
+  console.log("path :", req.path);
+  console.log("hostname :", req.hostname);
+  console.log("method :", req.method);
+  next(); // next function is used to tell node to move on to next bit of code explicitly as node by default doesn't continue executing with custom middleware like this
+});
 
 // middleware for static files - static fun is used to make express understand where the static files are and then express can look into the folders and make them public for us to use
-app.use(express.static('public'))
+app.use(express.static("public"));
 
-app.get('/',(req,res) => {
-    const blogs = [
-        {title: 'Hoshi finds plants', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-        {title: 'Kookie finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-        {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-      ];
-    res.render('index',{title : 'Home',blogs})
-})
+// mongoose and mongo snadbox routes
+// app.get("/add-blog", (req, res) => {
+//     const blog = new Blog({
+//         title: "Art and Nature",
+//         snippet: "Explore the intersection of art and nature, featuring works that celebrate the beauty of the natural world and inspire creativity.",
+//         desc: "Nature-Inspired Art is a creative expression that draws inspiration from the natural world, often using natural materials and forms to create unique and thought-provoking pieces. This art form seeks to connect us with our beautiful earth and its rhythms, encouraging us to appreciate the beauty and wonder of the natural world."
+//     })
 
-app.get('/about',(req,res) => {
-    res.render('about',{title : 'About'})
-})
- 
-app.get('/blogs/create',(req,res) => {
-    res.render('create',{title : 'Create'})
-})
- 
-app.use((req,res) => {
-    res.status(404).render('error',{title : '404'})
-})
- 
+//     blog.save().then((result) => {
+//         res.send(result);
+//     }).catch((err) => {
+//         console.log(err);
+//     })
+
+// })
+
+// get all blogs
+// app.get('/all-blogs', (req, res) => {
+//     Blog.find()
+//         .then((result) => {
+//             res.send(result);
+//         })
+//         .catch((err) => {
+//             console.log(err)
+//         })
+// });
+
+// find one blog
+// app.get("/single-blog", (req, res) => {
+//     Blog.findById('663e090ba6e30d6abacdc24c')
+//         .then((result) => {
+//             res.send(result);
+//         })
+//         .catch((err) => {
+//             console.log(err)
+//         })
+// })
+
+app.get("/", (req, res) => {
+  // const blogs = [
+  //     {
+  //         title: "Hoshi finds plants",
+  //         snippet: "Lorem ipsum dolor sit amet consectetur",
+  //     },
+  //     {
+  //         title: "Kookie finds stars",
+  //         snippet: "Lorem ipsum dolor sit amet consectetur",
+  //     },
+  //     {
+  //         title: "How to defeat bowser",
+  //         snippet: "Lorem ipsum dolor sit amet consectetur",
+  //     },
+  // ];
+  // res.render("index", { title: "Home", blogs });
+  res.redirect("/blogs");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", { title: "About" });
+});
+
+app.get("/blogs", (req, res) => {
+  Blog.find()
+    .then((result) => {
+      res.render('index',{title:'All-Blogs', blogs:result});
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.get("/blogs/create", (req, res) => {
+  res.render("create", { title: "Create" });
+});
+
+app.use((req, res) => {
+  res.status(404).render("error", { title: "404" });
+});
--- Mongodb/readme.md
+++ Mongodb/readme.md
@@ -3,3 +3,28 @@ Creating and Testing Mongodb atlas cloud version - https://www.mongodb.com/cloud
 Created a db user, a cluster and a db with a collection names blogs.
 
 learned how to connect with connection strings.
+
+---
+
+# Schemas and Models
+
+A db schema defines the structure or the types of data/document , sets the properties and property types
+
+example :
+Blog Schema
+
+- title (string), requried
+- snippet (string), requried
+- body(string ), requried
+
+// above will be the structure of the documents stored inside of our database
+
+*********
+Models - A db model allows us to communicate with the database collections.
+
+A model which is created off of a db schema consists of both static and instance methods which will help us to save, delete, update and get data from db.
+
+![alt text](image.png)
+
+# use mongeese to connect to db 
+pip install mongoose
