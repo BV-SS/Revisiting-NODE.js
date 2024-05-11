c54b7b2cd2dbb43dcb8c1f2989438727cf8d5186
Bharti Verma <bhartivermak2012@gmail.com>
2024-05-11 17:35:58 +0530

    Mongodb saving and getting data

--- CRUD_Operations/app.js
+++ CRUD_Operations/app.js
@@ -7,7 +7,7 @@ const app = express();
 // connect to mongodb and listen for requests when connected to db
 
 const dbURI =
-  "mongodb+srv://<user>:<password>@node-tuts.4fyd3ob.mongodb.net/Node?retryWrites=true&w=majority&appName=Node-tuts";
+  "mongodb+srv://<user>:<pass>@node-tuts.4fyd3ob.mongodb.net/Node?retryWrites=true&w=majority&appName=Node-tuts";
 
 mongoose
   .connect(dbURI)
@@ -35,8 +35,8 @@ app.use((req, res, next) => {
 // middleware for static files - static fun is used to make express understand where the static files are and then express can look into the folders and make them public for us to use
 app.use(express.static("public"));
 
-// middleware to get access to form data 
-app.use(express.urlencoded({extended:true}));
+// middleware to get access to form data
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.redirect("/blogs");
@@ -45,7 +45,6 @@ app.get("/", (req, res) => {
 app.get("/about", (req, res) => {
   res.render("about", { title: "About" });
 });
-
 
 app.get("/blogs/create", (req, res) => {
   res.render("create", { title: "Create" });
@@ -57,7 +56,7 @@ app.get("/blogs/create", (req, res) => {
 app.get("/blogs", (req, res) => {
   Blog.find()
     .then((result) => {
-      res.render('index',{title:'All-Blogs', blogs:result});
+      res.render("index", { title: "All-Blogs", blogs: result });
     })
     .catch((err) => {
       console.log(err);
@@ -65,27 +64,40 @@ app.get("/blogs", (req, res) => {
 });
 
 // save new blog
-app.post('/create-new-blog', (req,res) => {
-   const blog = new Blog(req.body);
+app.post("/create-new-blog", (req, res) => {
+  const blog = new Blog(req.body);
 
-   blog.save()
-   .then((result) => {
-      res.redirect('/blogs');
-   })
-   .catch((err) => {
-    console.log(err)
-   })
-})
+  blog
+    .save()
+    .then((result) => {
+      res.redirect("/blogs");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 // get one blog
-app.get('/blogs/:id', (req,res) =>{
+app.get("/blogs/:id", (req, res) => {
   const id = req.params.id;
   Blog.findById(id)
-  .then((result) => {
-    res.render("details",{title:'Detail Blog',blog:result})
-  })
-  .catch(err => console.log(err));
-})
+    .then((result) => {
+      res.render("details", { title: "Detail Blog", blog: result });
+    })
+    .catch((err) => console.log(err));
+});
+
+// delete one blog
+app.delete("/delete-blog/:id", (req, res) => {
+  const id = req.params.id;
+  Blog.findByIdAndDelete(id)
+    .then((response) => {
+      res.json({ redirect: "/blogs" });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 app.use((req, res) => {
   res.status(404).render("error", { title: "404" });
